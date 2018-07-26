803d19e42b6e3e51892c5fa4bc8b8c382953939d
igya0716 <igor.yadrov@netcracker.com>
2018-07-26 18:23:57 +0300

    handle labyrinth partly

--- check.py
+++ check.py
@@ -1,6 +1,12 @@
 import generator
 import runnability
 
-labyrinth = generator.generate(10, 0.75)
+labyrinth = [
+    [1, 1, 1, 1, 1],
+    [0, 0, 1, 1, 0],
+    [0, 0, 0, 0, 0],
+    [1, 1, 0, 1, 1],
+    [1, 1, 1, 1, 0]
+]
 generator.print_labyrinth(labyrinth)
-print(runnability.way_exist(labyrinth))
+print(runnability.way_exist(labyrinth, 2))
--- runnability.py
+++ runnability.py
@@ -1,39 +1,96 @@
-def way_exist(labyrinth):
+def way_exist(labyrinth, width):
     n = len(labyrinth)
-    for i in range(n):
+    count = int(n / width)
+    entries = set()
+    exits = set()
+    for i in range(len(labyrinth)):
         if labyrinth[i][0] == 0:
-            result = check_way(labyrinth, i, 0, i, 0)
-            if result:
-                return True
-    return False
+            entries.add(i)
+    if count > 0:
+        for i in range(count):
+            border = {'left_border': i * width,
+                      'right_border': (i + 1) * width}
+            if i == (count - 1):
+                border['right_border'] = len(labyrinth)
+            for row in entries:
+                check_way(labyrinth, border, {'current_row': row,
+                                              'current_column': i * width,
+                                              'old_row': row,
+                                              'old_column': i * width
+                                              }, exits)
+                print('iteration number ' + str(i) +
+                      ' left border is ' + str(border['left_border']) + ' right border is ' + str(border['right_border']))
+                print(exits)
+            entries = exits.copy()
+            exits.clear()
+    return len(entries) > 0
 
 
-def check_way(labyrinth, current_row, current_column, old_row, old_column):
-    up_row = current_row - 1
+def check_way(labyrinth, border, position, entries):
+    up_row = position['current_row'] - 1
     n = len(labyrinth)
-    result = False
+    current_row = position['current_row']
+    current_column = position['current_column']
+    old_row = position['old_row']
+    old_column = position['old_column']
+    left_border = border['left_border']
+    right_border = border['right_border']
     if up_row >= 0 and not (up_row == old_row and current_column == old_column):
         if labyrinth[up_row][current_column] == 0:
-            if current_column == n - 1:
-                result = True
+            if current_column == right_border - 1:
+                entries.add(up_row)
             else:
-                result = check_way(labyrinth, up_row, current_column, current_row, current_column)
+                print('from [' + str(current_row) + '][' + str(current_column) + ']'
+                      + ' to [' + str(up_row) + '][' + str(current_column) + ']')
+                labyrinth[current_row][current_column] = 1
+                check_way(labyrinth, border, {
+                    'current_row': up_row,
+                    'current_column': current_column,
+                    'old_row': current_row,
+                    'old_column': current_column
+                }, entries)
+
     down_row = current_row + 1
-    if not result and down_row <= (n - 1) and not (down_row == old_row and current_column == old_column):
+    if down_row <= (n - 1) and not (down_row == old_row and current_column == old_column):
         if labyrinth[down_row][current_column] == 0:
             if current_column == n - 1:
-                result = True
+                entries.add(down_row)
             else:
-                result = check_way(labyrinth, down_row, current_column, current_row, current_column)
+                print('from [' + str(current_row) + '][' + str(current_column) + ']'
+                      + ' to [' + str(down_row) + '][' + str(current_column) + ']')
+                labyrinth[current_row][current_column] = 1
+                check_way(labyrinth, border, {
+                    'current_row': down_row,
+                    'current_column': current_column,
+                    'old_row': current_row,
+                    'old_column': current_column
+                }, entries)
+
     left_column = current_column - 1
-    if not result and left_column >= 0 and not (current_row == old_row and left_column == old_column):
+    if left_column >= left_border and not (current_row == old_row and left_column == old_column):
         if labyrinth[current_row][left_column] == 0:
-            result = check_way(labyrinth, current_row, left_column, current_row, current_column)
+            print('from [' + str(current_row) + '][' + str(current_column) + ']'
+                  + ' to [' + str(current_row) + '][' + str(left_column) + ']')
+            labyrinth[current_row][current_column] = 1
+            check_way(labyrinth, border, {
+                'current_row': current_row,
+                'current_column': left_column,
+                'old_row': old_row,
+                'old_column': old_column
+            }, entries)
+
     right_column = current_column + 1
-    if not result and right_column <= (n - 1) and not (current_row == old_row and right_column == old_column):
+    if right_column <= right_border and not (current_row == old_row and right_column == old_column):
         if labyrinth[current_row][right_column] == 0:
-            if right_column == n - 1:
-                result = True
+            if right_column == right_border - 1:
+                entries.add(current_row)
             else:
-                result = check_way(labyrinth, current_row, right_column, current_row, current_column)
-    return result
+                print('from [' + str(current_row) + '][' + str(current_column) + ']'
+                      + ' to [' + str(current_row) + '][' + str(right_column) + ']')
+                labyrinth[current_row][current_column] = 1
+                check_way(labyrinth, border, {
+                    'current_row': current_row,
+                    'current_column': right_column,
+                    'old_row': old_row,
+                    'old_column': old_column
+                }, entries)
